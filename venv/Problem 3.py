4307cf4e621337a3a7c1afddc682825889b87d83
Chris Horr <chorr96@gmail.com>
2021-08-08 22:58:35 -0400

    Proposed solution to Daily Coding Problem 3. Serialize works properly, but I have not fixed the problem with the right trees disappearing on deserialization.

---
+++ venv/Problem 3.py
@@ -0,0 +1,44 @@
+# Good morning!
+# This is your coding interview problem for today.
+# This problem was asked by Google.
+# Given the root to a binary tree, implement serialize(root), which serializes the tree into a string,
+# and deserialize(s), which deserializes the string back into the tree.
+# For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def serialize(root):
+    s = ""
+    if root.left:
+        s = s + serialize(root.left)
+    else:
+        s = s + '#,'
+    if root.right:
+        s = s + serialize(root.right)
+    else:
+        s = s + '#,'
+    s = root.val + "," + s
+    return s
+
+def deserialize(s):
+    nodes = s.split(',', 1)
+    node2 = nodes[1].split(',', 1)
+    if nodes[0] == '#':
+        if node2[0] == '#':
+            return None
+        else:
+            return (None, Node(node2[0], deserialize(node2[1])))
+    else:
+        return Node(nodes[0],deserialize(nodes[1]))
+
+# The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+assert deserialize(serialize(node)).right.val == 'right'
+
+print(serialize(node))
