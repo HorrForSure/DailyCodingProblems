d4648145c5d30f4df1028b5a11ff1a81ee921aae
Chris Horr <chorr96@gmail.com>
2021-08-12 23:10:34 -0400

    Proposed solution to Daily Coding Problem 8. Completed without checking solution, this algorithm should only be called on each node in the tree once. The outer function is an easy way to prune the final 'unival' from being returned - if needed, this value would represent whether the entire tree is unival.

---
+++ venv/Problem 8.py
@@ -0,0 +1,54 @@
+# Good morning!
+# This is your coding interview problem for today.
+# This problem was asked by Google.
+# A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+# Given the root to a binary tree, count the number of unival subtrees.
+# For example, the following tree has 5 unival subtrees:
+#    0
+#   / \
+#  1   0
+#     / \
+#    1   0
+#   / \
+#  1   1
+
+# Assuming this definition of Node:
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def unival_trees(root):
+    def unival_help(node):
+        count = 0
+        unival = True
+        val = node.val
+        if (not node.left) & (not node.right):
+            a, b = 1, True
+        elif not node.right:
+            if node.left.val != val:
+                unival = False
+            a, b = unival_help(node.left)
+        elif not node.left:
+            if node.right.val != val:
+                unival = False
+            a, b = unival_help(node.right)
+        else:
+            a, b = unival_help(node.left)
+            c, d = unival_help(node.right)
+            a += c
+            b = b & d
+            if node.left.val == node.right.val == val:
+                if b:
+                    count += 1
+            else:
+                unival = False
+        count += a
+        unival = unival & b
+        return count, unival
+    total, unival = unival_help(root)
+    return total
+
+node = Node(0, Node(1), Node(0, Node(1, Node(1), Node(1)), Node(0, Node(0), Node(0))))
+print(unival_trees(node))
